93047b0c3ea0fb0f489327ae913f24162a810bbe
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-07-23 12:26:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// const obj = {
+//     name: 'Behruz',
+//     age: 1000,
+//     gruppa: ['Behruz', 'Azimjon', 'Anasxon']
+// }
+// obj.age = 1488
+
+// console.log(obj.age);
+
+let ism = prompt("Ism kiriting");
+let email = prompt("ID kiriting");
+let parol = prompt("Parol kiriting");
+const obj = {
+  name: ism,
+  id: email,
+  parol: parol,
+}
+console.log(obj);
